4f500eb582ffb2afa155acc6b1ae233488e8f0ac
Elias Dolinsek <elias.dolinsek@gmail.com>
2021-01-31 11:42:41 +0100

    TodoItem repository

---
+++ app/src/main/java/com/eliasdolinsek/simpletodo/domain/Repository.kt
@@ -0,0 +1,47 @@
+package com.eliasdolinsek.simpletodo.domain
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import java.util.*
+
+private const val TODO_ITEMS_KEY = "todoItems"
+
+class Repository(private val sharedPreferences: SharedPreferences, private val gson: Gson) {
+
+    fun add(todoItem: TodoItem) {
+        val todoItems = getAll()
+        todoItems.add(todoItem)
+        write(todoItems)
+    }
+
+    fun remove(id: UUID) {
+        val todoItems = getAll()
+        todoItems.removeIf { it.id == id }
+        write(todoItems)
+    }
+
+    fun update(id: UUID, update: TodoItem) {
+        val todoItems = getAll()
+        todoItems.indexOfFirst { it.id == id }.let {
+            todoItems.removeAt(it)
+            todoItems.add(it, update.copy(id = id))
+        }
+
+        write(todoItems)
+    }
+
+    private fun write(todoItems: List<TodoItem>) {
+        val todoItemsJson = gson.toJson(todoItems)
+        sharedPreferences.edit().putString(TODO_ITEMS_KEY, todoItemsJson).apply()
+    }
+
+    fun getAll(): MutableList<TodoItem> {
+        val todoItemsStr = sharedPreferences.getString(TODO_ITEMS_KEY, null)
+        todoItemsStr?.apply {
+            return gson.fromJson(todoItemsStr, List::class.java)
+                .toMutableList() as MutableList<TodoItem>
+        }
+
+        return mutableListOf()
+    }
+}
--- app/src/main/java/com/eliasdolinsek/simpletodo/domain/TodoItem.kt
+++ app/src/main/java/com/eliasdolinsek/simpletodo/domain/TodoItem.kt
@@ -1,11 +1,12 @@
-package com.eliasdolinsek.simpletodo.models
+package com.eliasdolinsek.simpletodo.domain
 
 import java.util.*
 
 data class TodoItem(
-    val id: UUID,
     val name: String,
     val description: String,
     val deadline: Date,
-    val done: Boolean
+    val done: Boolean,
+    val id: UUID = UUID.randomUUID()
 )
+
